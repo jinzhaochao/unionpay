0f370fbb6ffcbdeac5f4849f5aa147a44ebaaa3a
jinzhao <jinchao33@126.com>
2020-02-12 09:56:39 +0800

    删除

--- portalSupervisionService/supervision-web/src/main/java/com/unionpay/sms/utils/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.primeton.com/SMSMessageServiceService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.unionpay.sms.utils;
--- portalSupervisionService/supervision-web/src/main/java/com/unionpay/supervision/service/SuperLogService.java
+++
@@ -1,9 +0,0 @@
-package com.unionpay.supervision.service;
-
-import com.unionpay.supervision.domain.SuperLog;
-
-public interface SuperLogService {
-	
-	public void add(SuperLog log);
-
-}
